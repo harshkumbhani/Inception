1ecb098950e2c498486845a44b89857daeb8e91f
Harsh Kumbhani <Harshwut@icloud.com>
2024-05-13 21:01:45 +0200

    Removed usage of openrc, hence script for mariadb has no more while loops

--- srcs/requirements/mariadb/tools/startup.sh
+++ srcs/requirements/mariadb/tools/startup.sh
@@ -1,20 +1,12 @@
 #!/bin/sh
 
-rc-service mariadb start
-
-while ! mysqladmin ping --silent; do
-	sleep 1
-done
-
-mariadb -u root <<EOF
+if [ ! -d /var/lib/mysql/mariadb ]; then
+echo "
 CREATE DATABASE IF NOT EXISTS mariadb;
 CREATE USER IF NOT EXISTS 'hkumbhan'@'%' IDENTIFIED BY '1234';
 GRANT ALL PRIVILEGES ON mariadb.* TO 'hkumbhan'@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'r00t';
 FLUSH PRIVILEGES;
-EOF
+" | /usr/bin/mariadbd --user=mysql --bootstrap
+fi
 
-# rc-service mariadb restart
-rc-service mariadb stop
-
-exec /usr/bin/mariadbd --datadir=/var/lib/mysql --user=mysql 
+exec /usr/bin/mariadbd --datadir=/var/lib/mysql --user=mysql
