4a703ffd69ea3d1795e774e0e99d45edd412ae74
Harsh <Harshwut@icloud.com>
2024-05-14 15:25:14 +0200

    Fixed mariadb container for creating the user + introduced env variables. Added yml file to run nginx and mariadb, next up: wordpress container

--- srcs/requirements/mariadb/tools/startup.sh
+++ srcs/requirements/mariadb/tools/startup.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
-if [ ! -d /var/lib/mysql/mariadb ]; then
-echo "
-CREATE DATABASE IF NOT EXISTS mariadb;
-CREATE USER IF NOT EXISTS 'hkumbhan'@'%' IDENTIFIED BY '1234';
-GRANT ALL PRIVILEGES ON mariadb.* TO 'hkumbhan'@'%';
-FLUSH PRIVILEGES;
-" | /usr/bin/mariadbd --user=mysql --bootstrap
+function create_sql_file()
+{
+	cat << EOF > bootstrap.sql
+	FLUSH PRIVILEGES;
+	CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
+	CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PWD}';
+	GRANT ALL PRIVILEGES on \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
+	FLUSH PRIVILEGES;
+EOF
+}
+
+function run_bootstrap()
+{
+	mariadbd --user=mysql --bootstrap < bootstrap.sql;
+	rm -f bootstrap.sql
+}
+if [ ! -d "/var/lib/mysql/${DB_NAME}" ]; then
+	create_sql_file
+	run_bootstrap
 fi
 
 exec /usr/bin/mariadbd --datadir=/var/lib/mysql --user=mysql
